af1c925692425d5ada65bcde04f932b67cd94303
carrjose27 <carrascojose27@gmail.com>
2025-08-11 16:56:50 -0400

    Delete esthetician-site directory

--- esthetician-site/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("bookNow").addEventListener("click", function() {
-  document.getElementById("bookingFormSection").style.display = "block";
-  window.scrollTo({ top: document.getElementById("bookingFormSection").offsetTop, behavior: "smooth" });
-});
